7f70f149a80fae8bd436ccee9f1beb28056721ef
veremeychukden <veremeychukden@gmail.com>
2021-11-17 23:24:45 +0200

    Add calculator route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = observer(()=> {
           <Nav/>
           <Switch>
               <Route exact path="/" component={Home}/>
+              <Route exact path="/calculator" component={Calculator}/>
           </Switch>
           <Footer/>
       </div>
